f030b80fe004b90013643810085e3826e17a0447
RICHARDAAH <richardhayim@hotmail.com>
2016-12-12 12:01:37 +0000

    gif styling

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
-window.onload();
+$(start);
 
-var chose = chose;
+//When the page loads I want the table cells to fadeIn for a few seconds and then fadeOut.
 
-start function(chose) {
-  document.getElementsByClassName('cell')
-  document.fadeIn
-};
+
+function start() {
+  var cells = document.getElementsByClassName('cell');
+  fadeInAndOut(cells);
+
+
+}
+
+function fadeInAndOut(cells) {
+  for (var i = 0; i < cells.length; i++) {
+    var table = $(cells[i]);
+    table.fadeIn('slow');
+
+
+    setTimeout(function() {
+      console.log(table);
+      // bloop.toggleClass('tree');
+      $('.cell').removeClass('tree');
+      $('.cell').removeClass('snowman');
+    }, 3000);
+  }
+}
+
+//Now for the click function, want the cells background-image to appear after every click.
